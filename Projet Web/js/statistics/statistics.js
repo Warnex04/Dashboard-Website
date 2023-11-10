25a0a900811b750a878fa65f7c1e2d650507ddcd
Maximus <62805287+PapyRGB@users.noreply.github.com>
2023-11-10 16:35:20 +0100

    update

--- Projet Web/js/statistics/statistics.js
+++ Projet Web/js/statistics/statistics.js
@@ -1,88 +1,50 @@
-function createScatterPlot(data) {
-  // Clear any previous scatter plots
-  let container = document.querySelector('.scatterplot');
-  const existingCanvas = container.querySelector('canvas');
-  if (existingCanvas) {
-      existingCanvas.remove();
-  }
-  const canvas = document.createElement('canvas');
-  canvas.id = 'scatterPlot';
-  container.appendChild(canvas); // Append to the div with class 'scatterplot'
-  
-  // Filter and map the data for each variety
-  const setosaData = data.filter(item => item.variety.trim() === 'Setosa').map(item => ({
-      x: item['sepal.length'],
-      y: item['sepal.width']
-  }));
-  const versicolorData = data.filter(item => item.variety.trim() === 'Versicolor').map(item => ({
-      x: item['sepal.length'],
-      y: item['sepal.width']
-  }));
-  const virginicaData = data.filter(item => item.variety.trim() === 'Virginica').map(item => ({
-      x: item['sepal.length'],
-      y: item['sepal.width']
-  }));
-  
-  const scatterData = {
-      datasets: [{
-          label: 'Setosa',
-          data: setosaData,
-          backgroundColor: 'rgba(255, 99, 132, 0.4)',
-          borderColor: 'rgba(255, 99, 132, 1)'
-      }, {
-          label: 'Versicolor',
-          data: versicolorData,
-          backgroundColor: 'rgba(54, 162, 235, 0.4)',
-          borderColor: 'rgba(54, 162, 235, 1)'
-      }, {
-          label: 'Virginica',
-          data: virginicaData,
-          backgroundColor: 'rgba(75, 192, 192, 0.4)',
-          borderColor: 'rgba(75, 192, 192, 1)'
-      }]
-  };
-  
-  new Chart(canvas, {
-      type: 'scatter',
-      data: scatterData,
-      options: {
-          scales: {
-              x: {
-                  ticks: {
-                      color: 'white'
-                  },
-                  grid: {
-                      color: 'rgba(255, 255, 255, 0.7)',
-                      borderColor: 'white'
-                  },
-                  title: {
-                      display: true,
-                      text: 'Sepal Length (cm)',
-                      color: 'white'
-                  }
-              },
-              y: {
-                  ticks: {
-                      color: 'white'
-                  },
-                  grid: {
-                      color: 'rgba(255, 255, 255, 0.7)',
-                      borderColor: 'white'
-                  },
-                  title: {
-                      display: true,
-                      text: 'Sepal Width (cm)',
-                      color: 'white'
-                  }
-              }
-          },
-          plugins: {
-              legend: {
-                  labels: {
-                      color: 'white'
-                  }
-              }
-          }
-      }
+// Chart.register(BoxPlotController)
+$(document).ready(function(){
+  $('form input').change(function () {
+    $('form p').text(this.files.length + " file(s) selected");
   });
-  }
+});
+
+document.getElementById('dragForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    //readCSVFile()
+    
+    const fileInput = document.getElementById('file-post');
+    const file = fileInput.files[0];
+
+    if (file) {
+        Papa.parse(file, {
+            header: true,
+            dynamicTyping: true,
+            skipEmptyLines: true,
+            complete: function(results) {
+                console.log(results);
+                if (results.data.length === 0 || results.errors.length > 0) {
+                    alert('No data to display or there was an error parsing the CSV.');
+                    return;
+                }
+                
+                
+
+                
+                // Call the scatter plot creation function
+                //createScatterPlot(results.data);
+                
+                // Call the bar charts creation function
+                //createBarCharts(results.data);
+
+                // Call the pie charts creation function
+                //createPieChart(results.data)
+
+                // Call the radar charts creation function
+                //createRadarChart(results.data)
+
+                // Call the Boxplot charts creation function
+                // createBoxPlots(results.data)
+            }
+        });
+    } else {
+        alert('Please select a CSV file first.');
+    }
+});
+
